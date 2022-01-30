c4ad95b84b97777d8566cd56df32841bfe7e5d6f
Martin Schneider <martin@martingschneider.de>
2022-01-30 19:36:02 +0100

    add base and error graphql types

---
+++ src/graphql/types/base.ts
@@ -0,0 +1,14 @@
+import {GraphQLBoolean, GraphQLObjectType} from 'graphql';
+
+
+export const pageInfo = new GraphQLObjectType({
+	name: 'pageInfo',
+	fields: () => ({
+		hasNextPage: {
+			type: GraphQLBoolean
+		},
+		hasPreviousPage: {
+			type: GraphQLBoolean
+		}
+	})
+});
---
+++ src/graphql/types/error.ts
@@ -0,0 +1,14 @@
+import {GraphQLObjectType, GraphQLString} from 'graphql';
+
+
+export const errorNode = new GraphQLObjectType({
+	name: 'errorNode',
+	fields: () => ({
+		field: {
+			type: GraphQLString
+		},
+		message: {
+			type: GraphQLString
+		}
+	})
+});
