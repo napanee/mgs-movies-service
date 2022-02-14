15f58cd94865ac099de6e0d400023fea28737147
Martin Schneider <martin@martingschneider.de>
2022-02-14 14:06:42 +0100

    update apollo cache

--- client/js/index.tsx
+++ client/js/index.tsx
@@ -1,4 +1,5 @@
 import {ApolloClient, ApolloProvider, HttpLink, InMemoryCache} from '@apollo/client';
+import {relayStylePagination} from '@apollo/client/utilities';
 import {CssBaseline} from '@mui/material';
 import {StyledEngineProvider, ThemeProvider} from '@mui/material/styles';
 import {render} from 'react-dom';
@@ -10,7 +11,15 @@ import theme from './theme';
 
 
 const link = new HttpLink({uri: process.env.API_URL});
-const cache = new InMemoryCache();
+const cache = new InMemoryCache({
+	typePolicies: {
+		Query: {
+			fields: {
+				actors: relayStylePagination(),
+			},
+		},
+	},
+});
 const client = new ApolloClient({link, cache});
 
 render(
