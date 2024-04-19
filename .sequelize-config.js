b42d5aab4384c4996c9bf9134e49eef5f0d334e3
Martin Schneider <martin@martingschneider.de>
2024-04-19 17:25:40 +0200

    Update ci testrunner

--- .sequelize-config.js
+++ .sequelize-config.js
@@ -10,6 +10,13 @@ module.exports = {
 		host: '127.0.0.1',
 		dialect: 'postgres',
 	},
+	test: {
+		username: process.env.DATABASE_USER,
+		password: process.env.DATABASE_PASS,
+		database: process.env.DATABASE_NAME,
+		host: process.env.DATABASE_HOST,
+		dialect: process.env.DATABASE_DIALECT,
+	},
 	production: {
 		username: process.env.DATABASE_USER,
 		password: process.env.DATABASE_PASS,
